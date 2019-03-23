125a24394fc35c3b3ad4df7cc728bdc731dc24ce
unknown <kevlongalloway1999m@gmail.com>
2019-03-22 22:04:58 -0700

    update commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+ window.onscroll = function() {myFunction()};
+
+    function myFunction() {
+    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+        document.getElementById("top").className = "top-toggle fixed-right top-active";
+    } else {
+        document.getElementById("top").className = "top-toggle fixed-right";
+    }
+}
