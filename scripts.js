703691658bf782d964ea9700cebe5108cc51a970
unknown <kevlongalloway1999m@gmail.com>
2019-03-23 00:09:18 -0700

    optimization

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
  window.onscroll = function() {myFunction()};
-
     function myFunction() {
     if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
         document.getElementById("top").className = "top-toggle fixed-right top-active";
